33b1cf348552f911d9999997a7a7009e50404284
Ryan <dowdryan8@gmail.com>
2023-07-01 14:14:37 -0400

    Updated

--- sql-joins/data.sql
+++ sql-joins/data.sql
@@ -30,25 +30,3 @@ INSERT INTO vehicles (make, model, year, price, owner_id) VALUES ('Nissan', 'Alt
 INSERT INTO vehicles (make, model, year, price, owner_id) VALUES ('Lexus', 'ES350', 1998, 1599.99, 6);
 INSERT INTO vehicles (make, model, year, price, owner_id) VALUES ('BMW', '300', 2012, 22999.99, 6);
 INSERT INTO vehicles (make, model, year, price, owner_id) VALUES ('BMW', '700', 2015, 52999.99, 6);
-
-SELECT * FROM owners o
-FULL OUTER JOIN vehicles v
-ON o.id = v.owner_id;
-
-SELECT first_name, last_name, 
-  COUNT(owner_id) FROM owners o 
-  JOIN vehicles v on o.id=v.owner_id 
-  GROUP BY (first_name, last_name) 
-ORDER BY first_name;
-
-SELECT 
-  first_name, last_name, 
-  ROUND(AVG(price)) as average_price, 
-  COUNT(owner_id) 
-FROM owners o 
-JOIN vehicles v on o.id=v.owner_id 
-GROUP BY 
-  (first_name, last_name) 
-HAVING 
-  COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000 
-ORDER BY first_name DESC;
